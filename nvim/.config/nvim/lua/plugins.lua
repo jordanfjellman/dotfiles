1450becc5c8d353b0d5fa205cddbe4defa676565
Jordan Fjellman <jordan.fjellman@gmail.com>
2021-12-21 23:34:33 -0600

    Cleanup extra packer sync execution

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -48,7 +48,6 @@ return require("packer").startup(function(use)
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
     require("packer").sync()
   end
 end)
