ef171439df27208031cb6c5c10905bf79bb1e793
Jordan Fjellman <jordan.fjellman@gmail.com>
2021-12-21 23:34:09 -0600

    Remove unused cmp plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -13,7 +13,6 @@ return require("packer").startup(function(use)
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/cmp-path" },
-      { "hrsh7th/cmp-cmdline" },
       { "hrsh7th/cmp-vsnip" },
       { "hrsh7th/vim-vsnip" },
     },
