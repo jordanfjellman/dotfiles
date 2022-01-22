878b3ad9398a54e619dc119f520511128e2fc23f
Jordan Fjellman <jordan.fjellman@gmail.com>
2022-01-21 21:43:51 -0600

    Add toggleterm

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,6 +7,7 @@ end
 return require("packer").startup(function(use)
   use({ "wbthomason/packer.nvim", opt = true })
 
+  use({ "akinsho/toggleterm.nvim" })
   use({
     "hrsh7th/nvim-cmp",
     requires = {
