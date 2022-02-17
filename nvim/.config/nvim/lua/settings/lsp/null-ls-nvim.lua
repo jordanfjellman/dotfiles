a1b19a317ae93ff79eb20f57e26db3e84167e6f2
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2022-02-16 22:20:40 -0600

    Add prettier and eslint sources to null-ls

--- nvim/.config/nvim/lua/settings/lsp/null-ls-nvim.lua
+++ nvim/.config/nvim/lua/settings/lsp/null-ls-nvim.lua
@@ -5,11 +5,13 @@ M.setup = function()
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
   local formatting = null_ls.builtins.formatting
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-  -- local diagnostics = null_ls.builtins.diagnostics
+   local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup({
     debug = true,
     sources = {
+      diagnostics.eslint,
+      formatting.prettier,
       formatting.scalafmt,
     },
     on_attach = function(client)
