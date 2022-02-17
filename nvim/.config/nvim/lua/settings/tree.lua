25f6febfecd5643ab9dd6a90e9bda28dab195184
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2022-02-16 23:22:57 -0600

    Disable netwr

--- nvim/.config/nvim/lua/settings/tree.lua
+++ nvim/.config/nvim/lua/settings/tree.lua
@@ -3,7 +3,9 @@ local M = {}
 M.setup = function()
   vim.g["nvim_tree_quit_on_open"] = 1
 
-  require("nvim-tree").setup()
+  require("nvim-tree").setup({
+    disable_netrw = true,
+  })
 end
 
 return M
