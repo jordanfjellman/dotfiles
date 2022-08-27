a1cb7c00e3533ee1872b4ba3b30ef96e8c267c6d
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2022-08-27 08:29:58 -0500

    Use vim.cmd directly for intellisense

--- nvim/.config/nvim/lua/easy-align.lua
+++ nvim/.config/nvim/lua/easy-align.lua
@@ -1,5 +1,2 @@
-local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
-
 -- Align Github-flavored markdown tables
-cmd 'au FileType markdown vmap <Leader><Bslash> :EasyAlign*<Bar><Enter>'
-
+vim.cmd 'au FileType markdown vmap <Leader><Bslash> :EasyAlign*<Bar><Enter>'
