4085f130f1c917fe61b5c335f20ee817f971f562
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2022-08-27 22:32:19 -0500

    Remove highlight config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,11 +5,3 @@ require("fjellyvim.diagnostic").setup()
 require("fjellyvim.diagnostic").setup_keymaps()
 require("fjellyvim.theme").setup()
 require("fjellyvim.lsp").setup_keymaps()
-
-----------------------------------
--- Highlight ---------------------
-----------------------------------
--- cmd([[hi! link LspCodeLens CursorColumn]])
--- cmd([[hi! link LspReferenceText CursorColumn]])
--- cmd([[hi! link LspReferenceRead CursorColumn]])
--- cmd([[hi! link LspReferenceWrite CursorColumn]])
