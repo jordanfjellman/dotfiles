cba0a220e25e0fd7a8c687d7bdb8dd534e469da7
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2023-11-25 22:28:43 -0600

    Ignore bloop files in telescope

--- nvim/.config/nvim/lua/fjellyvim/plugins/telescope.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/telescope.lua
@@ -18,7 +18,7 @@ return {
 
     telescope.setup({
       defaults = {
-        file_ignore_patterns = { "^.git/" },
+        file_ignore_patterns = { "^.git/", "^.bloop/" },
         file_previewer = previewers.vim_buffer_cat.new,
         file_sorter = require("telescope.sorters").get_fzy_sorter,
         grep_previewer = previewers.vim_buffer_vimgrep.new,
