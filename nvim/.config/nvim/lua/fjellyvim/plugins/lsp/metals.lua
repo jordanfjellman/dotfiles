4defaf25b18a28afc89c43740a1225ef32c0fb5a
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2023-11-25 22:50:21 -0600

    Add metals progress as native lsp handler

--- nvim/.config/nvim/lua/fjellyvim/plugins/lsp/metals.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/lsp/metals.lua
@@ -25,6 +25,28 @@ return {
       showInferredType = true,
     }
 
+    local function metals_status_handler(_, status, ctx)
+      -- https://github.com/scalameta/nvim-metals/blob/main/lua/metals/status.lua#L36-L50
+      local val = {}
+      if status.hide then
+        val = { kind = "end" }
+      elseif status.show then
+        val = { kind = "begin", message = status.text }
+      elseif status.text then
+        val = { kind = "report", message = status.text }
+      else
+        return
+      end
+      local info = { client_id = ctx.client_id }
+      local msg = { token = "metals", value = val }
+      -- call fidget progress handler
+      vim.lsp.handlers["$/progress"](nil, msg, info)
+    end
+
+    local handlers = {}
+    handlers["metals/status"] = metals_status_handler
+    Metals_config.handlers = handlers
+
     Metals_config.on_attach = function(_, bufnr)
       local metals_lsp_group = vim.api.nvim_create_augroup("CustomMetalsLsp", { clear = true })
       vim.api.nvim_create_autocmd({ "BufWritePre" }, {
