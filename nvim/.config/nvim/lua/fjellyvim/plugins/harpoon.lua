6ea763557997ed0c0d20df32e2e525a249711123
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-01-04 22:33:42 -0600

    Switch harpoon keymaps to faster keymaps

--- nvim/.config/nvim/lua/fjellyvim/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/harpoon.lua
@@ -3,20 +3,29 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     menu = {
-    -- use a dynamic width for the popup menu
+      -- use a dynamic width for the popup menu
       width = vim.api.nvim_win_get_width(0) - 4,
-    }
+    },
   },
-  config = function ()
+  config = function()
     local ui = require("harpoon.ui")
-    vim.keymap.set("n", "<leader>ma", function() require("harpoon.mark").add_file() end)
-    vim.keymap.set("n", "<leader>mv", function() ui.toggle_quick_menu() end)
-    vim.keymap.set("n", "<leader>m1", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<leader>m2", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<leader>m3", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<leader>m4", function() ui.nav_file(4) end)
-    vim.keymap.set("n", "<leader>mn", function() ui.nav_next() end)
-    vim.keymap.set("n", "<leader>mp", function() ui.nav_next() end)
-  end
+    vim.keymap.set("n", "<leader>a", function()
+      require("harpoon.mark").add_file()
+    end)
+    vim.keymap.set("n", "<C-E>", function()
+      ui.toggle_quick_menu()
+    end)
+    vim.keymap.set("n", "<C-G>", function()
+      ui.nav_file(1)
+    end)
+    vim.keymap.set("n", "<C-H>", function()
+      ui.nav_file(2)
+    end)
+    vim.keymap.set("n", "<C-T>", function()
+      ui.nav_file(3)
+    end)
+    vim.keymap.set("n", "<C-Y>", function()
+      ui.nav_file(4)
+    end)
+  end,
 }
-
