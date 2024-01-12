9fe7750e5cc6a84fd1cdf6732d75df2ed41bd3ca
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-01-12 08:03:50 -0600

    Setup keymap for accepting copilot to not conflict autocompletion accept

--- nvim/.config/nvim/lua/fjellyvim/plugins/copilot.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/copilot.lua
@@ -2,6 +2,12 @@ return {
   "github/copilot.vim",
   lazy = false,
   config = function()
+    vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {
+      desc = "Copilot: Accept",
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
   end,
 }
