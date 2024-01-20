18d950dde07f229186d074a8d616f46e7223444f
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-01-20 12:42:24 -0600

    Reorder copilot config

--- nvim/.config/nvim/lua/fjellyvim/plugins/copilot.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/copilot.lua
@@ -2,12 +2,11 @@ return {
   "github/copilot.vim",
   lazy = false,
   config = function()
-    vim.keymap.set("i", "<C-J>", 'copilot#Accept("<CR>")', {
-      desc = "Copilot: Accept",
+    vim.g.copilot_assume_mapped = true
+    vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
       expr = true,
       replace_keycodes = false,
     })
     vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
   end,
 }
