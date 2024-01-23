aa47376a2605bbeac9bd012f439b386c270f1f11
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-01-22 21:34:11 -0600

    Remove luasnip

--- nvim/.config/nvim/lua/fjellyvim/plugins/cmp.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/cmp.lua
@@ -3,16 +3,6 @@ return {
     "hrsh7th/cmp-nvim-lsp", -- you also need to setup the completions for the lsp servers
   },
   {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    run = "make install_jsregexp",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-    config = function() end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       { "hrsh7th/cmp-buffer" },
@@ -22,7 +12,6 @@ return {
     },
     config = function()
       local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
 
       local kind_icons = {
         Text = "󰉿",
@@ -50,6 +39,7 @@ return {
         Event = "",
         Operator = "󰆕",
         TypeParameter = "",
+        Copilot = "",
       }
 
       local has_words_before = function()
@@ -59,12 +49,6 @@ return {
       end
 
       cmp.setup({
-        snippet = {
-          expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
         enabled = function()
           -- disable completion in comments
           local context = require("cmp.config.context")
@@ -81,25 +65,18 @@ return {
             vim_item.menu = ({
               buffer = "[Buffer]",
               latex_symbols = "[LaTeX]",
-              luasnip = "[LuaSnip]",
               nvim_lsp = "[LSP]",
               nvim_lua = "[Lua]",
-              vsnip = "[VSnip]",
             })[entry.source.name]
             return vim_item
           end,
         },
         mapping = {
-          -- WARNING:
-          -- You can't have select = true here _unless_ you are also using snippets.
-          -- Keep in mind that if you remove snippets you need to remove this select.
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<CR>"] = cmp.mapping.confirm(),
           ["<Tab>"] = cmp.mapping(function(fallback)
-            local copilot_keys = vim.fn["copilot#Accept"]()
+            -- local copilot_keys = vim.fn["copilot#Accept"]()
             if cmp.visible() then
               cmp.select_next_item()
-            elseif copilot_keys ~= "" and type(copilot_keys) == "string" then
-              vim.api.nvim_feedkeys(copilot_keys, "n", true)
             elseif has_words_before() then
               cmp.complete()
             else
@@ -114,6 +91,9 @@ return {
             else
               fallback()
             end
+            cmp.complete()
+            cmp.mapping.select_prev_item()
+            cmp.complete()
           end, { "i", "s" }),
           ["<C-E>"] = cmp.mapping.abort(),
         },
@@ -124,7 +104,6 @@ return {
               return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
             end,
           },
-          { name = "luasnip" },
           { name = "nvim_lsp_signature_help" },
         }, {
           { name = "buffer", keyword_length = 5 },
@@ -132,25 +111,10 @@ return {
         view = {
           entries = "custom", -- can be "custom", "wildmenu" or "native"
         },
+        experimental = {
+          ghost_text = false, -- this feature conflict with copilot.vim's preview.
+        },
       })
-
-      local ls = require("luasnip")
-
-      vim.keymap.set({ "i" }, "<C-K>", function()
-        ls.expand()
-      end, { silent = true })
-      vim.keymap.set({ "i", "s" }, "<C-L>", function()
-        ls.jump(1)
-      end, { silent = true })
-      vim.keymap.set({ "i", "s" }, "<C-J>", function()
-        ls.jump(-1)
-      end, { silent = true })
-
-      vim.keymap.set({ "i", "s" }, "<C-E>", function()
-        if ls.choice_active() then
-          ls.change_choice(1)
-        end
-      end, { silent = true })
 
       -- Set configuration for specific filetype.
       cmp.setup.filetype("gitcommit", {
