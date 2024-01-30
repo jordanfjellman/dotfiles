975b51464ec7495aec46f4ad2246ace912734356
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-01-29 22:37:35 -0600

    Customize lualine

--- nvim/.config/nvim/lua/fjellyvim/plugins/lualine.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/lualine.lua
@@ -1,5 +1,24 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons', optional = true },
-  opts = true,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", optional = true },
+  config = function()
+    require("lualine").setup({
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "filetype" },
+        lualine_y = { "diagnostics" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
+  end,
 }
