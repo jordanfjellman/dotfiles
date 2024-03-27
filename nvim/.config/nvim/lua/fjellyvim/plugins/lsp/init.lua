87300e15f2d5d2033db85625744721b79915472b
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-03-26 20:51:58 -0500

    Add typescript error plugin to neovim

--- nvim/.config/nvim/lua/fjellyvim/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/lsp/init.lua
@@ -1,3 +1,8 @@
 return {
+  -- Automatically configure Lua Language Server for Neovim development
   { "folke/neodev.nvim", opts = {} },
+
+  -- A Neovim port of Matt Pocock's ts-error-translator for VSCode for turning
+  -- messy and confusing TypeScript errors into plain English.
+  { "dmmulroy/ts-error-translator.nvim", opts = {} },
 }
