14682e2a87fafd53d1f249fb4e41e54b29cb078e
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-03-26 20:59:15 -0500

    Combine diagnostic plugin configs

--- nvim/.config/nvim/lua/fjellyvim/plugins/diagnostics.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/diagnostics.lua
@@ -32,5 +32,35 @@ return {
     dependencies = { "mfussenegger/nvim-dap" },
     events = { "VeryLazy" },
   },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require("trouble").toggle()
+        end,
+      },
+      {
+        "<leader>tn",
+        function()
+          require("trouble").next({ skip_groups = true, jump = true })
+        end,
+      },
+      {
+        "<leader>tp",
+        function()
+          require("trouble").previous({ skip_groups = true, jump = true })
+        end,
+      },
+      {
+        "gR",
+        function()
+          require("trouble").toggle("lsp_references")
+        end,
+      },
+    },
+  },
 }
-
