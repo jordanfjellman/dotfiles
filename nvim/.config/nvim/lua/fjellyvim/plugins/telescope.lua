fe491af10c00f7e5211135df745ad06ed2964b14
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-06-06 06:50:41 -0500

    Ensure make is installed for telescope

--- nvim/.config/nvim/lua/fjellyvim/plugins/telescope.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/telescope.lua
@@ -3,10 +3,18 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
-		-- todo: ensure make is installed
-		-- info: had to run make manually after switching from fzy
-		-- https://github.com/nvim-telescope/telescope-fzf-native.nvim/issues/47#issuecomment-988353015
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			-- `build` is used to run some command when the plugin is installed/updated.
+			-- This is only run then, not every time Neovim starts up.
+			build = "make",
+
+			-- `cond` is a condition used to determine whether this plugin should be
+			-- installed and loaded.
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
 		{ "ThePrimeagen/harpoon" },
 		{ "xiyaowong/telescope-emoji.nvim" },
 		{ "nvim-treesitter/nvim-treesitter" },
