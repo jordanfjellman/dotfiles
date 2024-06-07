b3606fdcb1ac1d3534334d72bd4eceb74ff1fb2e
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-06-07 16:27:43 -0500

    Setup biome to work alongside eslint

--- nvim/.config/nvim/lua/fjellyvim/plugins/linting.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/linting.lua
@@ -7,14 +7,49 @@ return {
   config = function()
     local lint = require("lint")
 
+    local biome_with_fallback = { "biomejs" }
+
     lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
+      json = { "biomejs" },
+      jsonc = { "biomejs" },
+      javascript = biome_with_fallback,
+      typescript = biome_with_fallback,
+      javascriptreact = biome_with_fallback,
+      typescriptreact = biome_with_fallback,
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    -- local lint = require "lint"
+    --
+    -- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    --
+    -- lint.linters_by_ft = {
+    -- typescript = { "biomejs", "eslint_d", "eslint" },
+    -- javascript = { "biomejs", "eslint_d", "eslint" },
+    -- typescriptreact = { "biomejs", "eslint_d", "eslint" },
+    -- javascriptreact = { "biomejs", "eslint_d", "eslint" },
+    -- }
+    --
+    -- local eslint = lint.linters.eslint_d
+    --
+    -- eslint.args = {
+    -- "--no-warn-ignored", -- <-- this is the key argument
+    -- "--format",
+    -- "json",
+    -- "--stdin",
+    -- "--stdin-filename",
+    -- function()
+    -- return vim.api.nvim_buf_get_name(0)
+    -- end,
+    -- }
+    --
+    -- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    -- group = lint_augroup,
+    -- callback = function()
+    -- lint.try_lint()
+    -- end,
+    -- })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
