4c7c575981681e400e8a9cd591849a2ebf0b6281
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-06-07 16:01:15 -0500

    Add todo comment highligher

--- nvim/.config/nvim/lua/fjellyvim/plugins/comment.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/comment.lua
@@ -1,20 +1,30 @@
 return {
-  "numToStr/Comment.nvim",
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter" },
-    -- Comment.nvim already supports treesitter out-of-the-box for all the languages except tsx/jsx.
-    { "JoosepAlviste/nvim-ts-context-commentstring" },
+  {
+    -- Highlight todo, notes, warnings, etc in comments
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
   },
-  lazy = false,
-  config = function()
-    require("Comment").setup({
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    })
+  {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter" },
+      -- Comment.nvim already supports treesitter out-of-the-box for all the languages except tsx/jsx.
+      { "JoosepAlviste/nvim-ts-context-commentstring" },
+    },
+    lazy = false,
+    config = function()
+      -- skip backwards compatibity checks to speed up loading
+      vim.g.skip_ts_context_commentstring_module = true
 
-    vim.g.skip_ts_context_commentstring_module = true
+      require("ts_context_commentstring").setup({
+        enable_autocmd = false,
+      })
 
-    require("ts_context_commentstring").setup({
-      enable_autocmd = false,
-    })
-  end,
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
 }
