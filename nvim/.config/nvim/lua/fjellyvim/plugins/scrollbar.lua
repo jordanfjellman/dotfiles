4a16765bdc3f14bdfaf4357af1a74e4083a5164d
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-06-07 07:04:57 -0500

    Add git changes to the scrollbar

--- nvim/.config/nvim/lua/fjellyvim/plugins/scrollbar.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/scrollbar.lua
@@ -1,17 +1,18 @@
 return {
-  "petertriho/nvim-scrollbar",
-  event = { "VeryLazy" },
-  dependencies = {
-    "kevinhwang91/nvim-hlslens", -- required to display search highlights
-  },
-  config = function()
-    require("scrollbar").setup({
-      marks = {
-        Search = {
-          color = "orange",
-        },
-      },
-    })
-    require("scrollbar.handlers.search").setup()
-  end,
+	"petertriho/nvim-scrollbar",
+	event = { "VeryLazy" },
+	dependencies = {
+		"kevinhwang91/nvim-hlslens", -- required to display search highlights
+	},
+	config = function()
+		require("scrollbar").setup({
+			marks = {
+				Search = {
+					color = "orange",
+				},
+			},
+		})
+		require("scrollbar.handlers.search").setup()
+		require("scrollbar.handlers.gitsigns").setup()
+	end,
 }
