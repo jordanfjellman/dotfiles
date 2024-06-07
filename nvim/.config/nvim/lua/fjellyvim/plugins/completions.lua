914037fe02102d4648de1ad7d11a6852de007b10
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2024-06-07 16:38:10 -0500

    Temporarily remove completion keymaps due to confict
    The "<C-h>" keymap was conflicting with the signature_help keymap.

--- nvim/.config/nvim/lua/fjellyvim/plugins/completions.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/completions.lua
@@ -69,19 +69,19 @@ return {
           --
           -- <c-l> will move you to the right of each of the expansion locations.
           -- <c-h> is similar, except moving you backwards.
-          ["<C-l>"] = cmp.mapping(function()
-            local luasnip = require("luasnip")
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-              cmp.mapping.confirm({ select = true })()
-            end
-          end, { "i", "s" }),
-          ["<C-h>"] = cmp.mapping(function()
-            local luasnip = require("luasnip")
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { "i", "s" }),
+          -- ["<C-l>"] = cmp.mapping(function()
+          -- 	local luasnip = require("luasnip")
+          -- 	if luasnip.expand_or_locally_jumpable() then
+          -- 		luasnip.expand_or_jump()
+          -- 		cmp.mapping.confirm({ select = true })()
+          -- 	end
+          -- end, { "i", "s" }),
+          -- ["<C-h>"] = cmp.mapping(function()
+          -- 	local luasnip = require("luasnip")
+          -- 	if luasnip.locally_jumpable(-1) then
+          -- 		luasnip.jump(-1)
+          -- 	end
+          -- end, { "i", "s" }),
 
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
