58c82dcf14491cd9e02b0d203b86c2d851e5d94f
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2025-09-19 16:53:25 -0500

    Don't use treesitter textobjects

--- nvim/.config/nvim/lua/fjellyvim/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/treesitter.lua
@@ -95,15 +95,6 @@ return {
           node_decremental = "<bs>",
         },
       },
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-        },
-      },
     },
     ---@param opts TSConfig
     config = function(_, opts)
