d6f8c2f2c93c3de351e152592e469cdadc6ecee5
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2025-11-17 07:07:20 -0600

    Cleanup unused plugin config from lsp init

--- nvim/.config/nvim/lua/fjellyvim/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/lsp/init.lua
@@ -1,20 +1,4 @@
 return {
-  -- {
-  --   "folke/lazydev.nvim",
-  --   ft = "lua", -- only load on lua files
-  --   opts = {
-  --     library = {
-  --       -- See the configuration section for more details
-  --       -- Load luvit types when the `vim.uv` word is found
-  --       { path = "luvit-meta/library", words = { "vim%.uv" } },
-  --       "nvim-dap",
-  --       "nvim-dap-ui",
-  --     },
-  --   },
-  -- },
-
-  -- { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-
   { "j-hui/fidget.nvim", opts = {} }, -- lsp notifications
 
   -- A Neovim port of Matt Pocock's ts-error-translator for VSCode for turning
