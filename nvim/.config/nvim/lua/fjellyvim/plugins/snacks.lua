6ea74fa80db961785dfb5efe560d3b00d3529648
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2025-11-17 06:56:27 -0600

    Update snacks config

--- nvim/.config/nvim/lua/fjellyvim/plugins/snacks.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/snacks.lua
@@ -46,8 +46,8 @@ return {
     -- find
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>ff", function() Snacks.picker.files({ hidden = true }) end, desc = "Find Files" },
+    { "<leader>fg", function() Snacks.picker.git_files({ hidden = true }) end, desc = "Find Git Files" },
     { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
     -- git
