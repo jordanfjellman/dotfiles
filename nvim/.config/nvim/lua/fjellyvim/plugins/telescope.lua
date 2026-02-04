258f8843bda612eb46c11b8a7b47d8add6524e11
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2026-02-04 06:40:24 -0600

    Minimize telescope config

--- nvim/.config/nvim/lua/fjellyvim/plugins/telescope.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/telescope.lua
@@ -1,66 +1,22 @@
 return {
   "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+  branch = "master",
+  -- Only load for Scala/Java files (for Metals integration)
+  ft = { "scala", "sbt", "java" },
   dependencies = {
     { "nvim-lua/plenary.nvim" },
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
-      build = "make",
-
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
-      cond = function() return vim.fn.executable("make") == 1 end,
-    },
-    { "ThePrimeagen/harpoon" },
-    { "nvim-treesitter/nvim-treesitter" },
-    { "nvim-tree/nvim-web-devicons" },
   },
   config = function()
     local telescope = require("telescope")
-    local previewers = require("telescope.previewers")
 
     telescope.setup({
       defaults = {
-        file_ignore_patterns = {
-          ".git/",
-          ".bloop/",
-          ".metals/",
-          ".yarn/",
-          "./bin/.local/share/",
-          "./bin/.local/state/",
-          "target/",
-          "samples/",
-        },
-        file_previewer = previewers.vim_buffer_cat.new,
-        file_sorter = require("telescope.sorters").get_fzy_sorter,
-        grep_previewer = previewers.vim_buffer_vimgrep.new,
-        layout_strategy = "vertical",
         prompt_prefix = "❯",
-      },
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = "smart_case",
-        },
       },
     })
 
-    telescope.load_extension("fzf")
-    telescope.load_extension("harpoon")
-
-    local builtin = require("telescope.builtin")
-
-    vim.keymap.set(
-      "n",
-      "<leader>sf",
-      function() builtin.find_files({ hidden = true }) end,
-      { desc = "[S]earch [F]iles" }
-    )
-
+    -- Load Metals extension for Scala LSP commands
+    -- Note: This extension is loaded by nvim-metals plugin
     vim.keymap.set(
       "n",
       "<leader>mc",
