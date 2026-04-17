e659a50b03fdac4f9bac8596b369646361718284
Jordan Fjellman <8724130+jordanfjellman@users.noreply.github.com>
2026-04-17 08:27:35 -0500

    Comment out nvim-treesitter use after archive

--- nvim/.config/nvim/lua/fjellyvim/plugins/comment.lua
+++ nvim/.config/nvim/lua/fjellyvim/plugins/comment.lua
@@ -6,25 +6,25 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = false },
   },
-  {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter" },
-      -- Comment.nvim already supports treesitter out-of-the-box for all the languages except tsx/jsx.
-      { "JoosepAlviste/nvim-ts-context-commentstring" },
-    },
-    lazy = false,
-    config = function()
-      -- skip backwards compatibity checks to speed up loading
-      vim.g.skip_ts_context_commentstring_module = true
-
-      require("ts_context_commentstring").setup({
-        enable_autocmd = false,
-      })
-
-      require("Comment").setup({
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      })
-    end,
-  },
+  -- {
+  --   "numToStr/Comment.nvim",
+  --   dependencies = {
+  --     { "nvim-treesitter/nvim-treesitter" },
+  --     -- Comment.nvim already supports treesitter out-of-the-box for all the languages except tsx/jsx.
+  --     { "JoosepAlviste/nvim-ts-context-commentstring" },
+  --   },
+  --   lazy = false,
+  --   config = function()
+  --     -- skip backwards compatibity checks to speed up loading
+  --     vim.g.skip_ts_context_commentstring_module = true
+  --
+  --     require("ts_context_commentstring").setup({
+  --       enable_autocmd = false,
+  --     })
+  --
+  --     require("Comment").setup({
+  --       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+  --     })
+  --   end,
+  -- },
 }
